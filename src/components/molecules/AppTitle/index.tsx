8251ac7e8ded55819e763da153e16b1bd19bd650
Jaume Castelló Sánchez <jaume97.cs@gmail.com>
2021-02-24 20:26:58 +0100

    AppTitle

--- src/components/molecules/AppTitle/index.tsx
+++ src/components/molecules/AppTitle/index.tsx
@@ -1,3 +1,5 @@
-const AppTitle = (): JSX.Element => <h1>Things I need to do</h1>
+import classes from './AppTitle.module.scss'
+
+const AppTitle = (): JSX.Element => <h1 className={classes.appTitle}>Things I need to do</h1>
 
 export default AppTitle
