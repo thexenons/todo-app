b4de4327392246143db5081a4a46d5747b3da595
Jaume Castelló Sánchez <jaume97.cs@gmail.com>
2021-02-24 20:26:45 +0100

    Basic Layout

--- src/components/objects/Layout/index.tsx
+++ src/components/objects/Layout/index.tsx
@@ -1,9 +1,10 @@
 import { ReactChild } from 'react'
+import classes from './Layout.module.scss'
 
 type LayoutProps = {
   children: ReactChild[]
 }
 
-const Layout = ({ children }: LayoutProps): JSX.Element => <div>{children}</div>
+const Layout = ({ children }: LayoutProps): JSX.Element => <div className={classes.layout}>{children}</div>
 
 export default Layout
