848cfe71da60d65722e68112a6b791876cf2f2b4
Jaume Castelló Sánchez <jaume97.cs@gmail.com>
2021-02-24 21:14:39 +0100

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import AppTitle from '@molecules/AppTitle'
+import './styles/global.scss'
+
 import Layout from '@objects/Layout'
 import SidebarList from '@organisms/SidebarList'
 import TodoList from '@organisms/TodoList'
-import './styles/global.scss'
 
 const App = (): JSX.Element => {
   return (
     <Layout>
-      <AppTitle />
       <SidebarList />
       <TodoList />
     </Layout>
--- src/components/objects/Layout/index.tsx
+++ src/components/objects/Layout/index.tsx
@@ -1,3 +1,5 @@
+import AppTitle from '@molecules/AppTitle'
+import Container from '@objects/Container'
 import { ReactChild } from 'react'
 import classes from './Layout.module.scss'
 
@@ -5,6 +7,13 @@ type LayoutProps = {
   children: ReactChild[]
 }
 
-const Layout = ({ children }: LayoutProps): JSX.Element => <div className={classes.layout}>{children}</div>
+const Layout = ({ children }: LayoutProps): JSX.Element => (
+  <div className={classes.layout}>
+    <AppTitle />
+    <Container>
+      <div className={classes.content}>{children}</div>
+    </Container>
+  </div>
+)
 
 export default Layout
