eb60c29568945b3d4bb56fa6352af32e82895ab8
Jaume Castelló Sánchez <jaume97.cs@gmail.com>
2021-02-24 21:58:29 +0100

    TodoListActions

--- src/components/molecules/TodoListActions/index.tsx
+++ src/components/molecules/TodoListActions/index.tsx
@@ -1,3 +1,10 @@
-const TodoListActions = (): JSX.Element => <div>TodoListActions</div>
+import classes from './TodoListActions.module.scss'
+
+const TodoListActions = (): JSX.Element => (
+  <div className={classes.todoListActions}>
+    <div>Clear completed tasks (0)</div>
+    <div>Delete list</div>
+  </div>
+)
 
 export default TodoListActions
--- src/components/organisms/TodoList/index.tsx
+++ src/components/organisms/TodoList/index.tsx
@@ -1,10 +1,11 @@
+import classes from './TodoList.module.scss'
 import List from '@molecules/List'
 import TodoListActions from '@molecules/TodoListActions'
 import Card from '@objects/Card'
 
 const TodoList = (): JSX.Element => (
-  <div>
-    <Card title="Active list" subtitle="Tasks remaining">
+  <div className={classes.todoList}>
+    <Card title="Active list" subtitle="Tasks remaining (0)">
       <List enableItemSeparators />
     </Card>
     <TodoListActions />
